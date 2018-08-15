bebde1c96b3286e11f2320a49d0aa0d6e5ed80ec
Albisetti <37214460+Albisetti@users.noreply.github.com>
2018-08-14 23:02:54 -0300

    BDD

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from Class_Categoria import Categorias
 from Class_Pelicula import Peliculas
 from Class_Personas import Autores, Actores, Productores
 from Class_Reviews import Reviews
-from flask import Flask, render_template
+from flask import request, jsonify, render_template, Flask
+from flask_jsglue import JSGlue
+import json
 import datetime
+import sys
 from Funciones import *
 
 data = Database()
@@ -12,6 +15,7 @@ data = Database()
 data.setConnection("127.0.0.1", "root", "alumno", "TPPYTHON")
 
 app = Flask(__name__)
+jsglue = JSGlue(app)
 
 tablas = nombreTablas()
 
@@ -20,8 +24,10 @@ def inicio():
 
     return render_template("inicio.html", urls = tablas)
 
-@app.route("/<tabla>")
+@app.route("/<tabla>", methods = ["GET", "POST"])
+
 def tabla(tabla):
+
         clase = globals()[tabla]
         var = clase()
         varauxinicial = clase()
@@ -30,10 +36,58 @@ def tabla(tabla):
         for item in aux:
             varauxfinal = varauxinicial.cargar(item[nombreID(tabla)])
             listaAux.append(varauxfinal)
+
         longitud = len(listaAux)
 
         return render_template("Tabla.html", name = tabla, select = Database().run("Select * FROM %s" % tabla),
                                row = Database().run("Select * FROM %s" % tabla).fetchall()[1],
                                variable = var, lista = listaAux, lenLista = longitud)
+
+@app.route("/borrar", methods = ["GET", "POST"])
+def borrar():
+    if request.method == "POST":
+
+        clase = globals()[request.get_json()['tabla']]
+        objetoinicial = clase()
+        objetofinal = objetoinicial.cargar(int(request.get_json()['id']))
+        objetofinal.baja()
+
+        print(data, file=sys.stderr)
+
+        return jsonify(data)
+
+    else:
+
+        return render_template("Tabla.html")
+
+
+
+@app.route("/<tabla>ingresar")
+def ingresar(tabla):
+
+    clase = globals()[tabla]
+    var = clase()
+    varauxinicial = clase()
+    aux = Database().run("Select * FROM %s" % tabla)
+    listaAux = []
+    for item in aux:
+        varauxfinal = varauxinicial.cargar(item[nombreID(tabla)])
+        listaAux.append(varauxfinal)
+    longitud = len(listaAux)
+
+    return render_template("ingresar.html", name=tabla, select=Database().run("Select * FROM %s" % tabla),
+                           row=Database().run("Select * FROM %s" % tabla).fetchall()[1],
+                           variable=var, lista=listaAux, lenLista=longitud)
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+'''
+function delete(id){
+             {% for cosa in range(lenLista)%}
+                if (id == {{cosa}}){
+                        {{ lista[cosa].baja() }}
+                    }
+             {% endfor %}
+        }
+'''
